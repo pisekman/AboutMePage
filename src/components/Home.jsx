f4503ba7ed62af61ce9df064717ba3c990d0b17c
pisekman <pisekman@gmail.com>
2023-03-11 18:15:37 +0100

    add base, home compoennts

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+const Home = () => {
+  return (
+    <>
+      <div className='tokyo_tm_home'>
+        <div className='home_content'>
+          <div className='avatar'>
+            <div
+              className='image avatar_img'
+              style={
+                {
+                  // backgroundImage: "url/assets/img/slider/1.jpg"
+                }
+              }
+            ></div>
+          </div>
+
+          <div className='details'>
+            <h3 className='name'>Michal Pisarski</h3>
+            <p className='job'>
+              Creative WebDeveloper based in Warsaw and happy to
+              travel all over Poland and Europe.
+            </p>
+            {/* social */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Social.jsx
@@ -0,0 +1,11 @@
+function Social() {
+  return (
+    <>
+      <p>facebook</p>
+      <p>instagram</p>
+      <p>linkedin</p>
+    </>
+  );
+}
+
+export default Social;
