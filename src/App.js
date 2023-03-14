b213b208ccdbb12ae9fc8d8f7d3428d536747dc8
pisekman <pisekman@gmail.com>
2023-03-14 20:42:04 +0100

    aos module test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ import { ToastContainer } from "react-toastify";
 function App() {
 
   useEffect(() => {
-    AOS.init();
+    AOS.init(
+
+      // {
+      //   offset: 200,
+      //   duration: 650,
+      //   easing: 'ease-in-sine',
+      //   delay: 100,
+      // }
+
+    );
   }, []);
   return (
     <div className="tokyo_tm_all_wrap">
